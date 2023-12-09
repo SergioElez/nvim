7f15098f15a62fca1ebaf684aa843f3768b227a0
SergioElez <sergioelezreche@gmail.com>
2023-12-09 23:49:14 +0100

    Mapeos EL DBUG FUNCIONA ATACHEA BIEN pero a veces hace cosas raras y no va pero normamente si

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -182,13 +182,6 @@ return {
   },
   -- Scroll
   {
-    'karb94/neoscroll.nvim',
-    config = function()
-      require'neoscroll'.setup({
-        })
-    end
-  },
-  {
     'petertriho/nvim-scrollbar',
     config = function()
       require'scrollbar'.setup({
